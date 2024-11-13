18280f0cc38acd557defe99f2141a8cca82ef48a
OlivierMarquis23 <olmar81@ulaval.ca>
2024-11-12 19:13:57 -0500

    Révision2 - fonctions dans quixo.py

--- quixo.py
+++ quixo.py
@@ -10,6 +10,7 @@ def formater_entête(joueurs):
     """
     return f"Légende:\n   X={joueurs[0]}\n   O={joueurs[1]}"
 
+
 def formater_le_damier(plateau):
     """
     Formate le plateau de jeu en art ASCII.
@@ -33,3 +34,20 @@ def formater_le_damier(plateau):
         "  | 1   2   3   4   5 |"
     )
     return damier
+
+
+def formater_le_jeu(joueurs, plateau):
+    entête = formater_entête(joueurs)
+    damier = formater_le_damier(plateau)
+    return f"{entête}\n{damier}"
+
+def choisir_un_coup():
+    origine = input("Donnez la position d'origine du cube (x,y) : ").split(',')
+    origine = [int(coord) for coord in origine]
+    direction = input("Quelle direction voulez-vous insérer? ('haut', 'bas', 'gauche', 'droite') : ")
+    return origine, direction
+
+def interpréter_la_commande():
+    parser = argparse.ArgumentParser(description="Quixo")
+    parser.add_argument('idul', help='IDUL du joueur')
+    return parser.parse_args()
