1e12bbdcb3dd9b6d956678fb223b486d64c57f9d
OlivierMarquis23 <olmar81@ulaval.ca>
2024-11-12 19:16:56 -0500

    Révision 4 - Test de api.py

--- api.py
+++ api.py
@@ -24,3 +24,22 @@ def initialiser_partie(idul, jeton):
     
     else:
         raise ConnectionError()
+    
+
+    def jouer_un_coup(id_partie, origine, direction, idul, jeton):
+    URL = f'https://pax.ulaval.ca/quixo/api/a24/partie/{id_partie}/'
+    payload = {
+        "origine": origine,
+        "direction": direction
+    }
+    response = requests.put(URL, auth=(idul, jeton), json=payload)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Réponse du serveur après un coup :", data)
+        id_partie = data.get('id')
+        etat = data.get('état')
+        gagnant = data.get('gagnant')
+        if gagnant:
+            raise StopIteration(gagnant)
+        return (id_partie, etat, etat.get('joueurs'))
